9f049746e610654d6eafebef169bbf43ce14fa89
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2019-03-19 11:55:08 +0300

    a

--- Android2/Resources/Resource.designer.cs
+++ Android2/Resources/Resource.designer.cs
@@ -9,9 +9,9 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: global::Android.Runtime.ResourceDesignerAttribute("XamarinAndroid1.Resource", IsApplication=true)]
+[assembly: global::Android.Runtime.ResourceDesignerAttribute("Android2.Resource", IsApplication=true)]
 
-namespace XamarinAndroid1
+namespace Android2
 {
 	
 	
